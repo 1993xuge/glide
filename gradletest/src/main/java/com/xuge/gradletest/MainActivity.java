c6e3cc0cb54826d69e65414ea0faaf7d7c01a3cd
yixu.wang <yixu.wang@ihandysoft.com>
2018-11-27 13:52:33 +0800

    简单使用

--- gradletest/src/main/java/com/xuge/gradletest/MainActivity.java
+++ gradletest/src/main/java/com/xuge/gradletest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.xuge.gradletest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        normalUse(this);
+    }
+
+    private void normalUse(Context context) {
+        String url = "http://img1.dzwww.com:8080/tupian_pl/20150813/16/7858995348613407436.jpg";
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(context)
+                .load(url)
+                .override(300, 300)
+//                .centerCrop()
+                .fitCenter()
+                .centerInside()
+                .into(imageView);
+    }
+
+    private void normalUseWithPlace(Context context) {
+        String url = "http://img1.dzwww.com:8080/tupian_pl/20150813/17/7858995348613407436.jpg";
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(context)
+                .load(url)
+                .placeholder(R.drawable.default_image)
+                .error(R.drawable.error_image)
+                .into(imageView);
     }
 }
